312ad5d82ab2113cbd5193c20f68d42000c9e6b6
Balaci Ionut <ionut.balaci@student.upt.ro>
2024-05-15 00:17:07 +0300

    add css to navbar

--- src/compontents/Navbar.js
+++ src/compontents/Navbar.js
@@ -3,27 +3,50 @@ import "./Navbar.css";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-  const [click, setClick] = useState(false);
+  const [click, setClick] = useState(true);
 
   const handleClick = () => setClick(!click);
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <ul>
+            <li>
+              <Link to="/" className="bid">
+                Bid
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="contact">
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link to="/" className="faq">
+                FAQ
+              </Link>
+            </li>
+          </ul>
+          <Link to="/" className="logo">
             BidBay
           </Link>
-          <ul>
-            <li>Bid</li>
-            <li>Contact</li>
-            <li>FAQ</li>
-          </ul>
-          <input placeholder="Search item"></input>
-          <i>Search</i>
-
-          <span>300$</span>
-          <i>My cart</i>
-          <img src="" alt="profile icon"></img>
+          <div className="search">
+            <input placeholder="Search item"></input>
+            <Link to="/" className="navbar-search">
+              <i className="fa-solid fa-magnifying-glass"></i>
+            </Link>
+          </div>
+          <Link to="/" className="navbar-balance">
+            <span>300$</span>
+          </Link>
+          <Link to="/" className="navbar-mycart">
+            <i className="fa-solid fa-cart-shopping" />
+          </Link>
+          <img
+            src={require("../images/Untitled.png")}
+            alt="profile icon"
+            className="profilePicture"
+          ></img>
           <div className="navbar-icon" onClick={handleClick}>
             <i
               className={click ? "fa-solid fa-bars" : "fa-solid fa-xmark-large"}
