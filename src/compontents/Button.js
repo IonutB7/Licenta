c1fdc208d4344e54438583d0b4d270007d47d08e
Balaci Ionut <ionut.balaci@student.upt.ro>
2024-05-16 04:03:19 +0300

    Update navbar

--- src/compontents/Button.js
+++ src/compontents/Button.js
@@ -1,2 +1,25 @@
 import React from "react";
 import "./Button.css";
+import { Link } from "react-router-dom";
+
+const BTN_STYLES = ["btn--typeOne", "btn--typeTwo"];
+const BTN_SIZES = ["btn--m", "btn--l"];
+
+export const Button = ({ children, onClick, type, btnStyle, btnSize }) => {
+  const checkBtnStyle = BTN_STYLES.includes(btnStyle)
+    ? btnStyle
+    : BTN_STYLES[0];
+  const checkBtnSize = BTN_SIZES.includes(btnSize) ? btnSize : BTN_SIZES[0];
+
+  return (
+    <Link to="sign-in" className="btn--mbl">
+      <button
+        className={`btn ${checkBtnStyle} ${checkBtnSize}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    </Link>
+  );
+};
--- src/compontents/Navbar.js
+++ src/compontents/Navbar.js
@@ -1,16 +1,28 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
+import { Button } from "./Button.js";
 
 function Navbar() {
   const [click, setClick] = useState(true);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener("resize", showButton);
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <ul>
+          <ul className="navbar-element">
             <li>
               <Link to="/" className="bid">
                 Bid
@@ -27,31 +39,42 @@ function Navbar() {
               </Link>
             </li>
           </ul>
-          <Link to="/" className="logo">
+          <Link to="/" className="logo navbar-element">
             BidBay
           </Link>
-          <div className="search">
-            <input placeholder="Search item"></input>
+          <form className="search navbar-element">
+            <input placeholder="Search item" name="item"></input>
             <Link to="/" className="navbar-search">
               <i className="fa-solid fa-magnifying-glass"></i>
             </Link>
-          </div>
-          <Link to="/" className="navbar-balance">
-            <span>300$</span>
+          </form>
+          <Link to="/" className="navbar-balance navbar-element">
+            <i className="fa-regular fa-dollar-sign" />
+            <span>300</span>
           </Link>
-          <Link to="/" className="navbar-mycart">
+          <Link to="/" className="navbar-mycart navbar-element">
             <i className="fa-solid fa-cart-shopping" />
+            <span>My cart</span>
           </Link>
           <img
             src={require("../images/Untitled.png")}
             alt="profile icon"
-            className="profilePicture"
+            className="profilePicture navbar-element"
           ></img>
-          <div className="navbar-icon" onClick={handleClick}>
+          <div className="navbar-icon navbar-element" onClick={handleClick}>
             <i
               className={click ? "fa-solid fa-bars" : "fa-solid fa-xmark-large"}
             />
           </div>
+          {button && (
+            <Button
+              className="navbar-element"
+              btnStyle="btn--typeOne"
+              btnSize="btn--l"
+            >
+              Login
+            </Button>
+          )}
         </div>
       </nav>
     </>
